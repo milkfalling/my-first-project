7043e7d06a1f6a6f6a54ca5a6abf87412f5bed89
Henry <hen85618@gmail.com>
2023-04-20 11:52:48 +0800

    我錯了我不應該亂打髒話面試官對不起幹

--- src/main/java/test/TestServlet.java
+++ src/main/java/test/TestServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append(";adsohgasoi;");
+		response.getWriter().append("Just monika!!");
 	}
 
 }
